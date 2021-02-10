4370427abcfe8ea54b03e0783408e0784535c7cb
chunthebear <zyc5026@hotmail.com>
2021-02-09 18:02:39 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# deep-q-learning-Minesweeper
+ 
---
+++ mibexx_gym/mibexx_gym_minesweeper/tests/main.py
@@ -0,0 +1,14 @@
+import gym
+
+env = gym.make('mibexx_gym_minesweeper:mibexx-gym-minesweeper-v0')
+env.reset()
+
+print("Observation space: ", env.observation_space.shape)
+print("Action space: ", env.action_space.nvec)
+
+state, reward, done, _ = env.step((0, 0))
+print(f"Reward: {reward} Done: {done}")
+state, reward, done, _ = env.step((0, 0))
+print(f"Reward: {reward} Done: {done}")
+
+env.render()
